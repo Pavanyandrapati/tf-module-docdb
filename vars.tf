e445f8ef3dc88d60885fd5bb5a8cbb6726efc5dd
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-06-04 17:09:16 +0530

    frontend

--- vars.tf
+++ vars.tf
@@ -9,8 +9,8 @@ variable "allow_db_cidr" {}
 variable "engine_version" {}
 variable "kms_arn" {}
 variable "port_no" {
-  default = "27017"
+  default = 27017
 }
-
-variable "instance_class" {}
 variable "instance_count" {}
+variable "instance_class" {}
+
